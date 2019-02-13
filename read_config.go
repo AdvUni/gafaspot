44b88922fdd66beafc5bd69ca3b8dd451c5bf4cf
Marie Lohbeck <lohbeck@au.de>
2019-02-13 14:35:56 +0100

    made compilable

--- read_config.go
+++ read_config.go
@@ -66,21 +66,30 @@ func initSecretEngines(config GafaspotConfig) map[string][]vault.SecretEngine {
 
 }
 
-func initDB(config GafaspotConfig) {
+func initDB(config GafaspotConfig) *sql.DB {
 	db, err := sql.Open("sqlite3", config.Database)
 	if err != nil {
 		log.Fatal("Not able to open database: ", err)
 	}
 
 	// Create table reservations. If it already exists, don't overwrite
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS reservations (id INTEGER PRIMARY KEY, status TEXT NOT NULL, username TEXT NOT NULL, env_name TEXT NOT NULL, start DATETIME NOT NULL, end DATETIME NOT NULL, subject TEXT, labels TEXT, delete_on DATE NOT NULL);")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS reservations (id INTEGER PRIMARY KEY, status TEXT NOT NULL, username TEXT NOT NULL, env_name TEXT NOT NULL, start DATETIME NOT NULL, end DATETIME NOT NULL, subject TEXT, labels TEXT, delete_on DATE NOT NULL);")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Create table users. If it already exists, don't overwrite
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS users (username TEXT UNIQUE NOT NULL, ssh_pub_key BLOB, delete_on DATE NOT NULL);")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (username TEXT UNIQUE NOT NULL, ssh_pub_key BLOB, delete_on DATE NOT NULL);")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Create table environments. If it already exist, deltete it first. Someone might have updated the environment configurations before system restart. So we want to create this table from scratch.
-	_, err := db.Exec("DROP TABLE IF EXISTS environments);")
-	_, err := db.Exec("CREATE TABLE environments (env_name TEXT UNIQUE NOT NULL, has_ssh BOOLEAN NOT NULL, description TEXT);")
+	_, err = db.Exec("DROP TABLE IF EXISTS environments);")
+	_, err = db.Exec("CREATE TABLE environments (env_name TEXT UNIQUE NOT NULL, has_ssh BOOLEAN NOT NULL, description TEXT);")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Fill empty table environments with information from configuration file
 	for envName, envConf := range config.Environments {
@@ -91,7 +100,10 @@ func initDB(config GafaspotConfig) {
 				envHasSSH = true
 			}
 		}
-		_, err := db.Exec("INSERT INTO environments VALUES (?, ?, ?);", envName, envHasSSH, envDescription)
+		_, err = db.Exec("INSERT INTO environments VALUES (?, ?, ?);", envName, envHasSSH, envDescription)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	return db
