3e9d8c36bb48a8cf7b5d57cf226c47d66f4229a1
Marie Lohbeck <lohbeck@au.de>
2019-02-19 14:10:59 +0100

    correction in sql statement

--- main.go
+++ main.go
@@ -27,23 +27,23 @@ func main() {
 
 	handleBookings(db, environments, approle)
 
-	stmt, err := db.Prepare("INSERT INTO reservations (upcoming, username, env_name, start, end, deletion_date) VALUES(?,?,?,?,?);")
+	stmt, err := db.Prepare("INSERT INTO reservations (status, username, env_name, start, end, delete_on) VALUES(?,?,?,?,?);")
 	if err != nil {
 		log.Fatal(err)
 	}
-	res, err := stmt.Exec("some_user", "demo0", "2019-02-14 00:00:00", "2019-02-15 00:00:00", "2020-02-15 00:00:00")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(res)
-
-	res, err = stmt.Exec("some_user", "demo1", "2019-02-14 00:00:00", "2019-02-15 00:00:00", "2020-02-15 00:00:00")
+	res, err := stmt.Exec("upcoming", "some_user", "demo0", "2019-02-14 00:00:00", "2019-02-15 00:00:00", "2020-02-15 00:00:00")
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println(res)
 
-	res, err = stmt.Exec("other_user", "demo0", "2019-02-12 00:00:00", "2019-02-14 10:00:00", "2020-02-15 00:00:00")
+	res, err = stmt.Exec("upcoming", "some_user", "demo1", "2019-02-14 00:00:00", "2019-02-15 00:00:00", "2020-02-15 00:00:00")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(res)
+
+	res, err = stmt.Exec("upcoming", "other_user", "demo0", "2019-02-12 00:00:00", "2019-02-14 10:00:00", "2020-02-15 00:00:00")
 	if err != nil {
 		log.Fatal(err)
 	}
