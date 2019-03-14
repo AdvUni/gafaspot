d0275ea80120f522901da8405032063d186db034
Marie Lohbeck <lohbeck@au.de>
2019-03-14 13:59:05 +0100

    add new file to repo

---
+++ database/save_key.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"log"
+	"time"
+)
+
+func SaveUserSSH(username string, ssh []byte) {
+	deleteOn := time.Now().AddDate(yearsTTL, 0, 0)
+	stmt, err := db.Prepare("REPLACE INTO table(username, ssh_pub_key, delete_on) VALUES(?,?,?);")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(username, ssh, deleteOn)
+	if err != nil {
+		log.Println(err)
+	}
+}
